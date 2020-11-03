bcf8c31d232fcfd0d6100d5e619ee5d3634bfdff
John Carlo Cunanan <codingjohn@outlook.com>
2020-11-03 11:48:39 +0800

    add start script

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, (req, res) => {
   console.log(`App is listening on http://localhost:${port}/`);
 });
